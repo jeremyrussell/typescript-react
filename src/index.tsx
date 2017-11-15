61b2566922e23b207572e723c933ee321d20832c
Jeremy Russell <me@jeremyrussell.com.au>
2017-11-15 11:32:58 +1100

    redux

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,21 @@ import * as ReactDOM from 'react-dom';
 // import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import Hello from './components/Hello';
+import Hello from './containers/Hello';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { enthusiasm } from './reducers/index';
+import { StoreState } from './types/index';
+
+const store = createStore<StoreState>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+});
 
 ReactDOM.render(
-  <Hello name="TypeScript" enthusiasmLevel={10} />,
-  document.getElementById('root') as HTMLElement
+  <Provider store={store}>
+  <Hello />
+</Provider>,
+document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
